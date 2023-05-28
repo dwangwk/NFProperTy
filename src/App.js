8cabd8e72c993012ef2d46bb8e0cf8087bd953ea
Daniel Wang <danielwang@Daniels-MacBook-Pro.local>
2023-05-28 21:32:20 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import PropertyForm from './Forms/PropertyForm';
+import './HomePage.css';
 
-function App() {
+const HomePage = () => {
+  const [properties, setProperties] = useState([]);
+  const [selectedProperty, setSelectedProperty] = useState(null);
+  const [showForm, setShowForm] = useState(false);
+  const [showListingForm, setShowListingForm] = useState(false);
+
+  useEffect(() => {
+    // Retrieve properties from local storage when the component mounts
+    const storedProperties = localStorage.getItem('properties');
+    if (storedProperties) {
+      setProperties(JSON.parse(storedProperties));
+    }
+  }, []);
+
+  useEffect(() => {
+    // Update local storage when properties state changes
+    localStorage.setItem('properties', JSON.stringify(properties));
+  }, [properties]);
+
+  const handlePropertyClick = (property) => {
+    setSelectedProperty(property);
+    setShowForm(true);
+  };
+
+  const handleFormSubmit = (tokens) => {
+    // Perform logic to handle user's token pledge
+    console.log(`User wants to buy ${tokens} tokens of property ${selectedProperty.realEstateName}`);
+    setShowForm(false);
+  };
+
+  const handleListingFormClick = () => {
+    setShowListingForm(true);
+  };
+
+  const handleListingFormSubmit = (property) => {
+    setProperties([...properties, property]);
+    setShowListingForm(false);
+  };
+
+  const renderProperties = () => {
+    if (properties.length === 0) {
+      return <p>No properties listed.</p>;
+    }
+
+    return (
+      <ul>
+        {properties.map((property, index) => (
+          <li key={index} onClick={() => handlePropertyClick(property)}>
+            <strong>{property.realEstateName}</strong>
+            <p>Token Price: {property.tokenPrice}</p>
+            <p>Total Tokens: {property.totalTokens}</p>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='home-page'>
+      <h2>Properties for Investment</h2>
+      {renderProperties()}
+      <button onClick={handleListingFormClick}>List a Property</button>
+      {showListingForm && (
+        <PropertyForm
+          onSubmit={handleListingFormSubmit}
+          onCancel={() => setShowListingForm(false)}
+        />
+      )}
+      {showForm && selectedProperty && (
+        <PropertyForm
+          property={selectedProperty}
+          onSubmit={handleFormSubmit}
+          onCancel={() => setShowForm(false)}
+        />
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default HomePage;
---
+++ src/context/AccountContext.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const AccountContext = React.createContext();
+
+function AccountContextProvider({ children }) {
+  const [account, setAccount] = React.useState("");
+
+  React.useEffect(() => {
+    const acc = sessionStorage.getItem("account");
+    setAccount(acc === "null" ? "" : acc);
+  }, []);
+
+  React.useEffect(() => {
+    sessionStorage.setItem("account", account);
+  }, [account]);
+
+  return (
+    <AccountContext.Provider value={[account, setAccount]}>
+      {children}
+    </AccountContext.Provider>
+  );
+}
+
+export { AccountContextProvider, AccountContext };
